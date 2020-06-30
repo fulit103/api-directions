3175274fb9a28613a7df0374e032f28a78ed7073
Julian Toro <jtoro@tecmovin.com>
2020-06-30 16:12:28 -0500

    tests e2e

---
+++ src/estimator/entrypoints/dto/route_dto.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from typing import Optional, List
+
+class PointDTO(BaseModel):
+  lat: float
+  lon: float
+
+class EstimateRouteDTO(BaseModel):
+  points: List[PointDTO]
+  country: str = "colombia"
+
+class ResponseRouteDTO(BaseModel):
+  distance: Optional[float] = None
+  time: Optional[float] = None
--- src/estimator/entrypoints/fastapi_app.py
+++ src/estimator/entrypoints/fastapi_app.py
@@ -1,1 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, status, HTTPException
+from fastapi.responses import JSONResponse
+from typing import List, Optional
+
+from estimator.config import Settings
+from estimator.entrypoints.dto import EstimateRouteDTO, ResponseRouteDTO
+from estimator.domain import Route, Point
+from estimator.aplication import RouteEstimator
+from estimator.infrastructure import RouteEstimatorGraphopperRequest
+
+settings = Settings()
+app = FastAPI()
+
+@app.post("/route", response_model=ResponseRouteDTO)
+def route(route_dto:EstimateRouteDTO, x_auth_token: Optional[str] = Header(None) ):
+  
+  if x_auth_token != settings.auth_token:
+    raise HTTPException(status_code=401, detail="Unauthorized user")
+
+  try:
+    points = [ Point(longitude=point.lon, latitude=point.lat) for point in route_dto.points]
+    route = Route(points)
+
+    request = RouteEstimatorGraphopperRequest(settings.graphhopper_api)
+    route_estimator = RouteEstimator(request)
+
+    response = route_estimator.estimate(route)
+    data = {"distance": response.distance, "time": response.time}
+    
+    return ResponseRouteDTO(**data)
+  except:
+    raise HTTPException(status_code=422, detail="Error de dominio")
+  
+
+  
+  
