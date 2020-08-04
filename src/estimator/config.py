fe617330c47ba307498f7ce83a8d437381192062
Julian Toro <jtoro@tecmovin.com>
2020-08-04 15:31:22 -0500

    token

--- src/estimator/config.py
+++ src/estimator/config.py
@@ -3,6 +3,6 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     graphhopper_api: str = "https://graphhopper.rapigo.co/route"
-    auth_token: str = "aAbdeerergssdgRCEdserdf"
+    auth_token: str = "tokenrapigo"
     google_matrix_key = "a"
     cors = "http://localhost"
