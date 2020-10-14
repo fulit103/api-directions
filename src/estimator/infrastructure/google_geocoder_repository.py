d1764be7e8cfe9f6e0fa6de9275630c4a10a4672
Julian Toro <jtoro@tecmovin.com>
2020-10-14 15:34:14 -0500

    client close geocodr

--- src/estimator/infrastructure/google_geocoder_repository.py
+++ src/estimator/infrastructure/google_geocoder_repository.py
@@ -8,17 +8,20 @@ from estimator.domain.adapters.geocoder_repository import GeocoderRepository
 class GoogleGeocoderRepository(GeocoderRepository):
 
     def __init__(self, key: str):
-        self._client: Client = Client(key)
+        self._key = key
 
     async def geocode(self, address: str) -> Optional[Point]:
-        response = await self._client.geocode(
-            address=address
-        )
+        async with Client(self._key) as client:
+            response = await client.geocode(
+                address=address
+            )
 
-        if len(response) == 0 or "geometry" not in response[0]:
-            return None
+            if len(response) == 0 or "geometry" not in response[0]:
+                return None
 
-        return Point(
-            latitude=response[0]["geometry"]["location"]["lat"],
-            longitude=response[0]["geometry"]["location"]["lng"]
-        )
+            return Point(
+                latitude=response[0]["geometry"]["location"]["lat"],
+                longitude=response[0]["geometry"]["location"]["lng"]
+            )
+
+        return None
