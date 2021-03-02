3172844469aba336f35deb322131815324ba6246
Julian David Toro <jtoro@tecmovin.com>
2021-03-02 08:46:37 -0500

    Update config.py

--- src/estimator/config.py
+++ src/estimator/config.py
@@ -3,8 +3,8 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     graphhopper_api: str = "https://graphhopper.rapigo.co/route"
-    auth_token: str = "tokenrapigo"
+    auth_token: str = "tokentoken"
     google_matrix_key = "a"
     google_geocoder_key = "a"
     cors = "http://localhost"
-    graphhopper_api_optimizate = "https://graphhopper.com/api/1/route?key=9cf6fbe5-2ab9-4c19-bdce-3ce707da3d92"
+    graphhopper_api_optimizate = "https://graphhopper.com/api/1/route?key=abc"
